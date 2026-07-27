fc9a89503e9a50c546320751f655404b394cdf3c
CoderGUY47 <shkhridoy92@gmail.com>
2026-07-28 00:00:00 +0600

    backend: add admin core dataset structures

---
+++ backend/Data/Admin/admin.ts
@@ -0,0 +1,13 @@
+export const SCH_DEPARTMENTS = [
+  "All Departments",
+  "Higher Mathematics",
+  "Physics",
+  "Chemistry",
+  "Biology",
+  "ICT & Computer Science",
+  "Bangla",
+  "English",
+  "Accounting & Finance",
+  "Economics",
+  "Administration & Accounts",
+];
